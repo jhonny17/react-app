6732e6ec0d2f3ed6f39f9235fca3477636b65108
Jhonny <jhnny.x17@gmail.com>
2022-02-26 17:32:49 -0600

    Using direct svg declaration

--- src/declarations/images.d.ts
+++ src/declarations/images.d.ts
@@ -1,13 +1,13 @@
-// declare module '*.svg';
+declare module '*.svg';
 declare module '*.png';
 declare module '*.gif';
 declare module '*.jpg';
 declare module '*.jpeg';
 
-declare module '*.svg' {
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const content: string;
+// declare module '*.svg' {
+//   const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+//   const content: string;
 
-  export { ReactComponent };
-  export default content;
-}
+//   export { ReactComponent };
+//   export default content;
+// }
